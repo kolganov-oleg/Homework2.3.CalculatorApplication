5d25a00f9a719162daaefb514893c397d0c28532
kolgan <6775366@gmail.com>
2022-09-23 03:20:20 +0300

    checking

---
+++ src/main/java/pro/sky/java/course2/calculatorapplication/service/CalculatorService.java
@@ -0,0 +1,8 @@
+package pro.sky.java.course2.calculatorapplication.service;
+
+public interface CalculatorService {
+    public int plus(Integer a, Integer b);
+    public int minus(Integer a, Integer b);
+    public int multiple(Integer a, Integer b);
+    public double divide(Integer a, Integer b);
+}
